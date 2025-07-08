6b433e38a05dc884a3dc8290c65a6751110d984a
Leonardo Vian Pereira <leonardoviana00@hotmail.com>
2025-07-08 18:55:39 -0300

    feat(project): base project created

---
+++ orderms-api/src/main/java/viana/tech/io/orderms_api/OrdermsApiApplication.java
@@ -0,0 +1,13 @@
+package viana.tech.io.orderms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermsApiApplication.class, args);
+	}
+
+}
