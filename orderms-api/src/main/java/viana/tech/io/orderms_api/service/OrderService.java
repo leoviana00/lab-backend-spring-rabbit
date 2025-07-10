a0d86210783ae4d7f3a47ad94562c6c3871695d9
Leonardo Vian Pereira <leonardoviana00@hotmail.com>
2025-07-10 09:53:27 -0300

    feat(service): Create customer order listing service

--- orderms-api/src/main/java/viana/tech/io/orderms_api/service/OrderService.java
+++ orderms-api/src/main/java/viana/tech/io/orderms_api/service/OrderService.java
@@ -3,34 +3,62 @@ package viana.tech.io.orderms_api.service;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.bson.Document;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
+
+import viana.tech.io.orderms_api.controller.dto.OrderResponse;
 import viana.tech.io.orderms_api.entity.OrderEntity;
 import viana.tech.io.orderms_api.entity.OrderItem;
-import viana.tech.io.orderms_api.listener.OrderCreatedListener;
 import viana.tech.io.orderms_api.listener.dto.OrderCreatedEvent;
 import viana.tech.io.orderms_api.repository.OrderRepository;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository,
+                        MongoTemplate mongoTemplate) {
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
-    public void save(OrderCreatedEvent event){
+    public void save(OrderCreatedEvent event) {
 
         var entity = new OrderEntity();
-        
-        entity.setOrderId(event.codigoCliente());
+
+        entity.setOrderId(event.codigoPedido());
         entity.setCustomerId(event.codigoCliente());
-        entity.setItems(getOrderItens(event));
+        entity.setItems(getOrderItems(event));
         entity.setTotal(getTotal(event));
 
         orderRepository.save(entity);
 
+    }
+
+    public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest) {
+        var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
+
+        return orders.map(OrderResponse::fromEntity);
+    }
+
+    public BigDecimal findTotalOnOrdersByCustomerId(Long customerId) {
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
     }
 
     private BigDecimal getTotal(OrderCreatedEvent event) {
@@ -39,10 +67,9 @@ public class OrderService {
                 .map(i -> i.preco().multiply(BigDecimal.valueOf(i.quantidade())))
                 .reduce(BigDecimal::add)
                 .orElse(BigDecimal.ZERO);
-
     }
 
-    private List<OrderItem> getOrderItens(OrderCreatedEvent event) {
+    private static List<OrderItem> getOrderItems(OrderCreatedEvent event) {
         return event.itens().stream()
                 .map(i -> new OrderItem(i.produto(), i.quantidade(), i.preco()))
                 .toList();
